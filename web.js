b8c7bad471adc77a7dc26937784f20be0205866a
diaz994 <vd31192@gmail.com>
2013-12-23 21:22:07 +0000

    Update web.js

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
+var fs = require("fs");
 var express = require('express');
 var app = express();
+var buf = new Buffer(256);
 app.use(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
+  var file = fs.readFileSync('index.html');
+  buf.write(file,"utf-8");
 });
 
 var port = process.env.PORT || 5000;
